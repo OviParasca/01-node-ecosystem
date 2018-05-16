f04d60cc7d288c0db3acb7d26581225e9fe37181
Ovi Parasca <OviParasca@gmail.com>
2018-05-16 16:09:19 -0700

    - fixed another double quotes issue

--- 01-node-ecosystem/lab-Ovi/index.js
+++ 01-node-ecosystem/lab-Ovi/index.js
@@ -3,6 +3,6 @@
 const greet = require('./lib/greet.js');
 const arithmetic = require('./lib/arithmetic.js');
 
-console.log(greet.sayHello("Ovi"));
+console.log(greet.sayHello('Ovi'));
 console.log(arithmetic.add(3, 2));
 console.log(arithmetic.sub(3, 2));
