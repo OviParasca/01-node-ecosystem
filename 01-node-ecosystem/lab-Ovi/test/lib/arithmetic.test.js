ecb168193c0eef71e9f121541fde4dd2739ef3f7
Ovi Parasca <OviParasca@gmail.com>
2018-05-16 15:53:12 -0700

    - trying to fix Travis

--- 01-node-ecosystem/lab-Ovi/test/lib/arithmetic.test.js
+++ 01-node-ecosystem/lab-Ovi/test/lib/arithmetic.test.js
@@ -2,9 +2,6 @@
 const arithmetic = require('../../lib/arithmetic.js');
 const assert = require('assert');
 
-
-
-
 assert.strictEqual(arithmetic.add(2, 3), 5, 'Sum is not correct');
 assert.strictEqual(arithmetic.add(2, "o"), null, 'Addition is not valid');
 
